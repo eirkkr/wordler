c8e98e493f098cd3e5c5fc63b2c9e19ade28cfcd
Eric Parkin <eric.parkin@protonmail.com>
2022-01-29 19:29:29 +0800

    Refactored wordler.py.

--- wordler.py
+++ wordler.py
@@ -1,17 +1,37 @@
+"""
+Gets list of all five letter words from 
+"""
+
+from typing import List
+
+
 def main() -> None:
 
-    # Read in words.
+    words = _load_words()
+    words = _get_five_letter_words(words)
+
+    print(words)
+
+
+def _load_words() -> List[str]:
+
     with open("words_alpha.txt") as file:
         words = file.readlines()
 
-    # Strip newline characters.
-    words = [word.strip() for word in words]
+    return words
 
-    five_letter_words = []
+
+def _get_five_letter_words(words: List[str]) -> List[str]:
+
+    five_letter_words: List[str] = []
 
     for word in words:
         if len(word) == 5:
             five_letter_words.append(word)
+
+    words = [word.strip() for word in words]
+
+    return five_letter_words
 
 
 if __name__ == "__main__":
